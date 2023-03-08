cfa2c936ab80e62b176f9c662a9225dd2f65e19b
BPluijm <baspluijm@gmail.com>
2023-03-08 21:48:33 +0100

    adjusted css navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import {MdOutlineTravelExplore} from 'react-icons/md';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import {TbGridDots} from 'react-icons/tb';
 
 const Navbar = () => {
+    const [active, setActive] = useState('navBar')
+    const showNav = () => {
+        setActive('navBar activeNavbar')
+    }
+
+    const removeNavbar = () => {
+        setActive('navBar')
+    }
+
   return (
     <section className='navBarSection'>
         <header className="header">
@@ -15,7 +24,7 @@ const Navbar = () => {
                 </a>
             </div>
 
-            <div className="navBar">
+            <div className={active}>
                 <ul className="navLists">
 
                    <li className="navItem">
@@ -51,11 +60,11 @@ const Navbar = () => {
                    </button>
                 </ul>
 
-                <div className="closeNavbar">
+                <div onClick={removeNavbar} className="closeNavbar">
                     <AiFillCloseCircle className="icon"/>
                 </div>
             </div>
-            <div className="toggleNavbar">
+            <div onClick={showNav} className="toggleNavbar">
                 <TbGridDots className="icon"/>
             </div>
 
