ffb5840f60da921b7e9b52f317762db028224ae8
BPluijm <baspluijm@gmail.com>
2023-03-10 22:07:45 +0100

    started on home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './home.css';
+import video from '../../Assets/video.mp4';
 
 const Home = () => {
   return (
-    <div>home</div>
+    <sextion className='home'>
+      <div className='overlay'></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className='homeContent container'>
+        <div className='textDiv'>
+          <span className='smallText'>Our Packages</span>
+          <h1 className='homeTitle'>Search your Holiday</h1>
+        </div>
+
+        <div className='cardDiv grid'>
+          <label htmlFor='city'> Search your destination:</label>
+          <div className input flex>
+            <input type='text' placeholder='Enter name here....' />
+          </div>
+        </div>
+      </div>
+
+    </sextion>
   )
 }
 
